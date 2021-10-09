d93d51afa9d4637e05dbd245c3c93e2e1d61a759
eduardobitar <eduardo@bitar.eti.br>
2021-10-09 12:56:05 -0300

    App Python v1.0

--- test.py
+++ test.py
@@ -16,5 +16,5 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "MESSAGE")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
 
